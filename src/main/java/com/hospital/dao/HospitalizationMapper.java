015b259bcb3575ecd0053e8b9f37dcdb18654c45
PUITO <2904340675@qq.com>
2021-07-18 21:11:10 +0800

    first commit

---
+++ src/main/java/com/hospital/dao/HospitalizationMapper.java
@@ -0,0 +1,27 @@
+package com.hospital.dao;
+
+import com.hospital.entity.Hospitalization;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Date;
+import java.util.List;
+
+public interface HospitalizationMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Hospitalization record);
+
+    int insertSelective(Hospitalization record);
+
+    Hospitalization selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Hospitalization record);
+
+    int updateByPrimaryKey(Hospitalization record);
+    List<Hospitalization> findAll(List<Integer> patientids, String intime);
+    List<Hospitalization> selectByPatientId(@Param("patientid")Integer patientId);
+    Hospitalization findTheLastHospitalization(@Param("id")Integer id);
+    List<Hospitalization> findOtherHospitalization(Hospitalization record);
+    int findPatientByBed(@Param("floor") int floor,@Param("door") int door,@Param("bed") int bed);
+
+}
---
+++ src/main/java/com/hospital/service/AppointmentService.java
@@ -0,0 +1,19 @@
+package com.hospital.service;
+
+import com.hospital.entity.Appointment;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface AppointmentService {
+    List<Appointment> getAllAppointments();
+    List<Appointment> getAllAppointments(String doctorname,String patientname);
+    String delAppointment(Integer id);
+    Appointment getAppointment(Integer id);
+    String UpdateAppointment(Appointment appointment);
+    String addAppointment(Appointment appointment);
+    List<Appointment> getPatientMessage(Integer patientId);
+    List<Appointment> selectByDoctorId(Integer doctorId,String patientname,String time);
+    Integer selectTheLastAppointment(Integer patientId);
+    Double getAppMoney(int num);
+}
